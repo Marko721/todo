230fbb30fdf537303a5fbad43a94592129310ca6
Marko721 <popmark1002@gmail.com>
2021-06-20 22:41:05 +0200

    fix

--- app.js
+++ app.js
@@ -9,24 +9,29 @@ todoButton.addEventListener("click", addTodo);
 function addTodo(event) {
   //Prevent form from submitting
   event.preventDefault();
+
   // ToDo Div
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
+
   // Create LI
   const newTodo = document.createElement('li');
   newTodo.innerText = todoInput.nodeValue;
   newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
+
   //Check mark button
   const completedButton = document.createElement('button');
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
   completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
+
   //Check trash button
   const trashButton = document.createElement('button');
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
   trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
+  
   // Append to list
   todoList.appendChild(todoDiv);
 }
